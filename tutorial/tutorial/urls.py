18bf8c45c23e778211d6596ca91e5aabbd3aac39
GennadiiTurutin <gennadii.turutin@gmail.com>
2018-11-28 17:24:30 -0500

    Added all files

---
+++ tutorial/tutorial/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from rest_framework import routers
+from tutorial.quickstart import views
+
+router = routers.DefaultRouter()
+router.register('users', views.UserViewSet)
+router.register('groups', views.GroupViewSet)
+
+urlpatterns = [
+    url('', include(router.urls)),
+    url('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    ]
